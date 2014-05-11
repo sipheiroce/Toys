d0bf32fa9d8fd62afd19e347684f8da102ed9007
Zuoning Yin <zyin@graphsql.com>
2014-05-10 22:29:13 -0400

    add v2 html text extractor

---
+++ html_text_extract/process2.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import re
+import os
+
+def process_html(fname):
+    f=open(fname, "r")
+    content = f.read()
+    f.close()
+
+    out_f = fname[:-5] + ".txt"
+    f = open(out_f, "w")
+    
+    soup = BeautifulSoup(content, "html5lib")
+    out_c = str(soup.h1.text) + "\n\n"
+
+    for dp in soup.find_all("p", class_="summaryText"):
+        out_c = out_c + str(dp.text) + "\n\n"
+
+
+    for d in soup.find_all(id=re.compile("mod-a-body")):
+        for p in d.find_all("p"):
+            out_c = out_c +  str(p)[3:-4] + "\n\n"
+  
+    f.write(out_c)
+    f.close()
+
+
+
+def process_all():
+    for f in os.listdir("."):
+        if f.endswith(".html"):
+            process_html(f)
+
+
+process_all()
